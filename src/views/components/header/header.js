51de20aa74aa41e8ffe5d873eec9410a068c4502
Pavel <p.tataurov92@gmail.co>
2019-10-16 11:04:21 +0300

    commit

--- src/views/components/header/header.js
+++ src/views/components/header/header.js
@@ -1,3 +1,12 @@
 import './header.scss'
 import './_imports/import'
-  
+import $ from 'jquery'
+
+$(document).ready(() => {
+	$('.header__logo').click(function() {
+		$('html, body').animate(
+			{ scrollTop: $(`${this.dataset.target}`).offset().top - 20 },
+			400
+		)
+	})
+})
