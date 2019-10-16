016ca84356aa398af1ac6e5d708056f2009a3e6f
Pavel <p.tataurov92@gmail.co>
2019-10-16 15:40:40 +0300

    commit

--- src/views/components/section-2/section-2.js
+++ src/views/components/section-2/section-2.js
@@ -5,12 +5,13 @@ import 'owl.carousel/dist/assets/owl.carousel.css'
 import 'owl.carousel'
 
 $(document).ready(() => {
-	const slider = $('.section-2__slider-items.owl-carousel').owlCarousel({
-		autoWidth: true,
-		dots: false
-	})
-
-	$('.section-2__slider-btn-next').click(() => {
-		slider.trigger('next.owl.carousel')
+	$(window).on('load', () => {
+		const slider = $('.section-2__slider-items.owl-carousel').owlCarousel({
+			autoWidth: true,
+			dots: false
+		})
+		$('.section-2__slider-btn-next').click(() => {
+			slider.trigger('next.owl.carousel')
+		})
 	})
 })
--- src/views/components/section-3/section-3.js
+++ src/views/components/section-3/section-3.js
@@ -5,24 +5,25 @@ import 'owl.carousel/dist/assets/owl.carousel.css'
 import 'owl.carousel'
 
 $(document).ready(() => {
-	const slider = $('.section-3__slider.owl-carousel').owlCarousel({
-		dots: false,
-		margin: 30,
-		items: 1,
-		mouseDrag: false,
-		touchDrag: false,
-		loop: true
-	})
+	$(window).on('load', () => {
+		const slider = $('.section-3__slider.owl-carousel').owlCarousel({
+			dots: false,
+			margin: 30,
+			items: 1,
+			mouseDrag: false,
+			touchDrag: false,
+			loop: true
+		})
+		$('.section-3__btn-more').click(() => {
+			slider.trigger('next.owl.carousel')
+		})
 
-	$('.section-3__btn-more').click(() => {
-		slider.trigger('next.owl.carousel')
-	})
-
-	$('.section-3__mobile-slider.owl-carousel').owlCarousel({
-		dots: false,
-		margin: 10,
-		items: 1,
-		autoWidth: true,
-		loop: true
+		$('.section-3__mobile-slider.owl-carousel').owlCarousel({
+			dots: false,
+			margin: 10,
+			items: 1,
+			autoWidth: true,
+			loop: true
+		})
 	})
 })
